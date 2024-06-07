5cde7c56f7bb9555548b71324a0ee80b73a7a4b4
Abhijeet <iamgenious21o9@gmail.com>
2024-06-07 18:58:10 +0530

    workupdate2

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ app.use((err, req, res, next) => {
   res.render("error",{error:err});
 });
 
-// app.listen(port, () => {
-//   console.log("App listening on port " + port);
-// });
+app.listen(port, () => {
+  console.log("App listening on port " + port);
+});
 
-module.exports = app;
