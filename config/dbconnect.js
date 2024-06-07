e0b10f541da60738553792975840aebb922ca765
Abhijeet <iamgenious21o9@gmail.com>
2024-06-07 17:00:06 +0530

    version1

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -4,11 +4,11 @@ var mongoDB = process.env.CONNECTION_STRING;
 const connectdb = async () => {
   try {
     const connect = await mongoose.connect(mongoDB);
-    console.log(
-      "DATABASE CONNECTED : ",
-      connect.connection.host,
-      connect.connection.name
-    );
+    // console.log(
+    //   "DATABASE CONNECTED : ",
+    //   connect.connection.host,
+    //   connect.connection.name
+    // );
   } catch (error) {
     console.log(error);
   }
